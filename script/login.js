23551f81f24823d000acb3f7ca3c99f8a53df6fd
Júlio <julio.dev.freitas@gmail.com>
2025-03-29 15:27:49 -0300

    Validação de email e senha do usuário no `login.js`

--- script/login.js
+++ script/login.js
@@ -2,8 +2,8 @@ const formulario = document.querySelector("#formulario");
 const email = document.querySelector("#email");
 const senha = document.querySelector("#senha");
 const mostrarSenha = document.querySelector("#mostrarSenha");
-let verificadorEmail = 0;
-let verificadorSenha = 0;
+
+
 
 mostrarSenha.addEventListener("click", (evt) => {
   evt.target.checked ? senha.type = "text" : senha.type = "password";
@@ -12,30 +12,34 @@ mostrarSenha.addEventListener("click", (evt) => {
 formulario.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const usuariosCadastrados = JSON.parse(localStorage.getItem("usuario"))
+  let emailCadastrado = false;
+  let senhaCorreta = false;
+
+  function redirecionarPagina(){
+    window.location.href = "../html/home.html";
+    email.value = "";
+    senha.value = "";
+  }
 
   for(let cadastro of usuariosCadastrados){
     if (email.value.toLowerCase() === cadastro.email) {
+      emailCadastrado = true;
+
       if(senha.value === cadastro.senha){
-        window.location.href = "../html/home.html";
-        email.value = "";
-        senha.value = "";
-      } else{
-        verificadorSenha ++;
+        senhaCorreta = true;
       }
-    } else {
-      verificadorEmail++;
-    }
+    } 
   }
 
-  if(verificadorEmail === usuariosCadastrados.length){
+  if(!emailCadastrado){
     alert("Email não cadastrado, por favor cadastre-se!");
-    verificadorEmail = 0;
     senha.value ="";
     email.focus();
-  }
-  if(verificadorSenha === usuariosCadastrados.length){
+  } else if(!senhaCorreta){
     alert("Senha incorreta!");
-    verificadorSenha = 0;
+    senhaCorreta = 0;
     senha.focus();
+  } else {
+    redirecionarPagina();
   }
 });
