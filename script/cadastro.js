18028ffd2c92682443327327c12f0e3bcb3c9ee5
Júlio <julio.dev.freitas@gmail.com>
2025-03-29 12:00:24 -0300

    Verificação de usuários ou emails já cadastrados no `cadastro.js`

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,57 +1,70 @@
-const formulario = document.querySelector("#formulario");
-const usuario = document.querySelector("#usuario");
 const email = document.querySelector("#email");
 const senha = document.querySelector("#senha");
-const confSenha = document.querySelector("#confSenha");
-const mostrarSenha = document.querySelector("#mostrarSenha")
-const mostrarConfirmacao = document.querySelector("#mostrarConfirmacao")
+const usuario = document.querySelector("#usuario");
+const formulario = document.querySelector("#formulario");
+const confirmacao = document.querySelector("#confirmacao");
+const mostrarSenha = document.querySelector("#mostrarSenha");
+const mostrarConfirmacao = document.querySelector("#mostrarConfirmacao");
+
+const cadastros = JSON.parse(localStorage.getItem("usuario"));
 
 const limparCampos = () =>{
   usuario.value ="";
   email.value ="";
   senha.value ="";
-  confSenha.value ="";
+  confirmacao.value ="";
   mostrarSenha.checked = false;
   mostrarConfirmacao.checked = false;
 }
 
-const exibir = (evt) => { evt.target.checked ? senha.type = "text" : senha.type = "password"};
-
-mostrarSenha.addEventListener("click", exibir);
-mostrarConfirmacao.addEventListener("click", exibir);
+const exibirSenha = (e) => { e.target.checked ? senha.type = "text" : senha.type = "password" };
+const exibirConfirmacao = (e) => { e.target.checked ? confirmacao.type = "text" : confirmacao.type = "password" };
+mostrarSenha.addEventListener("click", exibirSenha);
+mostrarConfirmacao.addEventListener("click", exibirConfirmacao);
 
 formulario.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  const usuarios = [];
-  const usuariosCadastrados = JSON.parse(localStorage.getItem("usuario"));
+  const usuariosNovos = [];
+  let usuarioRepetido = false;
+  let emailRepetido = false;
 
-  if(senha.value !== confSenha.value){
-    
-    formulario.removeEventListener("submit", e => e.preventDefault());
-    alert("Digite sua senha corretamente");
-    confSenha.focus(); 
+  if(cadastros === null){
+
+    usuariosNovos.push({nomeUsuario: usuario.value, email: email.value.toLowerCase(), senha: senha.value})
+    localStorage.setItem("usuario", JSON.stringify(usuariosNovos));
+    limparCampos();
 
   } else {
-    if(localStorage.getItem("usuario") === null) {
-      usuarios.push({usuario: usuario.value, email: email.value.toLowerCase(), senha: senha.value});
 
-      localStorage.setItem("usuario", JSON.stringify(usuarios));
-  
-      window.location.href = "../html/login.html"
-      limparCampos();
-      
-    } else {
-      for(let cadastro of usuariosCadastrados){
-        usuarios.push(cadastro);
+    for(let cadastro of cadastros){
+      usuariosNovos.push(cadastro);
+
+      if(usuario.value === cadastro.nomeUsuario){
+        usuarioRepetido = true;
+      } else if(email.value.toLowerCase() === cadastro.email) {
+        emailRepetido = true;
       }
+    }
 
-      usuarios.push({usuario: usuario.value, email: email.value.toLowerCase(), senha: senha.value});
-      localStorage.setItem("usuario", JSON.stringify(usuarios));
-  
-      window.location.href = "../html/login.html"
+    if(usuarioRepetido){
+      alert("Nome de usuário já existe!");
+      usuarioRepetido = false;
+      usuario.focus();
+
+    } else if (emailRepetido) {
+      alert("E-mail já cadastrado, utilize outro email");
+      emailRepetido = false;
+      email.focus();
+
+    } else {
+      usuariosNovos.push({nomeUsuario: usuario.value, email: email.value.toLowerCase(), senha: senha.value});
+      localStorage.setItem("usuario", JSON.stringify(usuariosNovos));
       limparCampos();
     }
+    
   }
 });
+
+//     window.location.href = "../html/login.html"
   
 
