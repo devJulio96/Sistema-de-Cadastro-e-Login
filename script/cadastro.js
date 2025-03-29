69c87f9d60fae22664e088fd453b6f7aec7f597a
Júlio <julio.dev.freitas@gmail.com>
2025-03-29 15:08:21 -0300

    Implementação da validação de senha no `cadastro.js`

--- script/cadastro.js
+++ script/cadastro.js
@@ -5,7 +5,6 @@ const formulario = document.querySelector("#formulario");
 const confirmacao = document.querySelector("#confirmacao");
 const mostrarSenha = document.querySelector("#mostrarSenha");
 const mostrarConfirmacao = document.querySelector("#mostrarConfirmacao");
-
 const cadastros = JSON.parse(localStorage.getItem("usuario"));
 
 const limparCampos = () =>{
@@ -27,13 +26,39 @@ formulario.addEventListener("submit", (evt) => {
   const usuariosNovos = [];
   let usuarioRepetido = false;
   let emailRepetido = false;
+  
+
+  function validarSenha(){
+    if(senha.value.length < 8) {
+      alert("A senha precisa ter no mínimo 8 dígitos");
+    } else if (senha.value.match(/[A-Z]/g) === null) {
+      alert("A senha precisa ter pelo menos uma letra maiúscula")
+    } else if(senha.value.match(/[0-9]/g) === null){
+      alert("A senha precisa ter pelo menos um número")
+    } else if(senha.value.match(/\W/g) === null) {
+      alert("A senha precisa ter pelo menos um caractere especial")
+    } else if(senha.value === confirmacao.value){
+      cadastrarUsuarioNovo();
+      redirecionarPagina();
+    } else {
+      confirmacao.focus();
+      alert("Digite sua senha corretamente");
+    }
+  }
+
+  function cadastrarUsuarioNovo(){
+    usuariosNovos.push({nomeUsuario: usuario.value, email: email.value.toLowerCase(), senha: senha.value});
+    localStorage.setItem("usuario", JSON.stringify(usuariosNovos));
+    alert("Cadastro feito com sucesso");
+    limparCampos(); 
+  }
+
+  function redirecionarPagina(){
+    window.location.href = "../html/login.html";
+  }
 
   if(cadastros === null){
-
-    usuariosNovos.push({nomeUsuario: usuario.value, email: email.value.toLowerCase(), senha: senha.value})
-    localStorage.setItem("usuario", JSON.stringify(usuariosNovos));
-    limparCampos();
-
+    validarSenha();
   } else {
 
     for(let cadastro of cadastros){
@@ -57,14 +82,11 @@ formulario.addEventListener("submit", (evt) => {
       email.focus();
 
     } else {
-      usuariosNovos.push({nomeUsuario: usuario.value, email: email.value.toLowerCase(), senha: senha.value});
-      localStorage.setItem("usuario", JSON.stringify(usuariosNovos));
-      limparCampos();
+      validarSenha();
     }
-    
   }
 });
 
-//     window.location.href = "../html/login.html"
+
   
 
